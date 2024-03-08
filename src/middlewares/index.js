bce17b7d8c2de52330991e60942c4e8fc9242cfe
Arash Altafi <arashaltafi1377@gmail.com>
2024-03-08 22:58:03 +0330

    fix cors

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,6 +2,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 module.exports = (app) => {
-    // app.use(cors);
+    app.use(cors());
     app.use(bodyParser.json());
 }
