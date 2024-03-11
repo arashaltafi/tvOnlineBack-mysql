d21bb218957262cf9f547dc738c7e5129002ab38
Arash Altafi <arashaltafi1377@gmail.com>
2024-03-12 00:51:53 +0330

    a

--- src/controllers/profileControllers.js
+++ src/controllers/profileControllers.js
@@ -1,4 +1,5 @@
 const mysql = require('../configs/db.config');
+const axios = require('axios');
 const { createImageFromBase64, generateRandomNumber } = require('../utils/Helper');
 
 const sendName = async (req, res, next) => {
@@ -49,6 +50,67 @@ const sendName = async (req, res, next) => {
 }
 
 const sendAvatar = async (req, res, next) => {
+    try {
+        const { image } = req.body;
+
+        if (!image) {
+            return res.status(400).send({
+                state: 'err',
+                message: 'لطفا عکس خود را ارسال نمایید'
+            })
+        }
+        if (typeof image !== 'string') {
+            return res.status(400).send({
+                state: 'err',
+                message: 'لطفا عکس را به صورت رشته ارسال نمایید'
+            })
+        }
+
+        const token = req.headers.authorization;
+
+        // Select User Phone
+        const [userPhone] = await mysql.query('SELECT phone FROM tbl_subscribers WHERE token = ?', [token])
+
+        const response = await axios({
+            url: 'https://arashaltafi.ir/tvonline/v1/send_profile_avatar2.php',
+            method: 'POST',
+            data: {
+                phone: userPhone[0].phone,
+                image: image
+            },
+        })
+
+        if (response.status !== 200) {
+            return res.status(500).send({
+                state: 'err',
+                message: 'خطا در انجام عملیات',
+            });
+        }
+
+        // Update Avatar User
+        await mysql.query('UPDATE tbl_subscribers SET avatar = ? WHERE token = ?', [response.data.avatar, token]);
+
+        // Select User Data
+        const [user] = await mysql.query('SELECT name, avatar FROM tbl_subscribers WHERE token = ?', [token])
+
+        return res.status(200).send({
+            state: 'ok',
+            message: 'عملیات موفق',
+            data: {
+                name: user[0].name,
+                avatar: user[0].avatar
+            }
+        });
+
+    } catch (error) {
+        return res.status(500).send({
+            state: 'err',
+            message: 'خطا در انجام عملیات',
+        });
+    }
+}
+
+const sendAvatar2 = async (req, res, next) => {
     try {
         const { image } = req.body;
 
